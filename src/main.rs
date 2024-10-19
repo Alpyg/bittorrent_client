c4dca277b04c3470ab21682bb8d2c42639471a71
Alpyg <alpyg@pm.me>
2024-10-19 03:21:34 -0400

    discover peers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::Context;
 use clap::{Parser, Subcommand};
-use sha1::{Digest, Sha1};
 use torrent::{Keys, Torrent};
+use tracker::{TrackerRequest, TrackerResponse};
 
 use std::path::PathBuf;
 
 mod bencode;
 mod torrent;
+mod tracker;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -19,9 +20,11 @@ struct Args {
 enum Command {
     Decode { value: String },
     Info { torrent: PathBuf },
+    Peers { torrent: PathBuf },
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
@@ -40,12 +43,7 @@ fn main() -> anyhow::Result<()> {
                 println!("Length: {length}")
             }
 
-            let info_encoded =
-                serde_bencode::to_bytes(&t.info).context("re-encode into section")?;
-            let mut hasher = Sha1::new();
-            hasher.update(&info_encoded);
-            let info_hash = hasher.finalize();
-            println!("Info Hash: {}", hex::encode(&info_hash));
+            println!("Info Hash: {}", hex::encode(t.info.hash()));
 
             println!("Piece Length: {}", t.info.piece_length);
             println!("Piece Hashes:");
@@ -53,7 +51,54 @@ fn main() -> anyhow::Result<()> {
                 println!("{}", hex::encode(&hash));
             }
         }
+        Command::Peers { torrent } => {
+            let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
+            let t: Torrent =
+                serde_bencode::from_bytes(&dot_torrent).context("parse torrent file")?;
+
+            let length = if let torrent::Keys::SingleFile { length } = t.info.keys {
+                length
+            } else {
+                todo!();
+            };
+
+            let request = TrackerRequest {
+                peer_id: "00112233445566778899".to_owned(),
+                port: 6881,
+                uploaded: 0,
+                downloaded: 0,
+                left: length,
+                compact: 1,
+            };
+            let url_params =
+                serde_urlencoded::to_string(&request).context("url-encode tracker parameters")?;
+
+            let response = reqwest::get(format!(
+                "{}?{}&info_hash={}",
+                t.announce,
+                url_params,
+                urlencode(&t.info.hash()),
+            ))
+            .await
+            .context("query tracker")?;
+            let response = response.bytes().await.context("fetch tracker response")?;
+            let response: TrackerResponse =
+                serde_bencode::from_bytes(&response).context("parse tracker response")?;
+
+            for peer in &response.peers.0 {
+                println!("{}:{}", peer.ip(), peer.port());
+            }
+        }
     }
 
     Ok(())
+}
+
+fn urlencode(t: &[u8; 20]) -> String {
+    let mut encoded = String::with_capacity(3 * t.len());
+    for &byte in t {
+        encoded.push('%');
+        encoded.push_str(&hex::encode(&[byte]));
+    }
+    encoded
 }
--- src/torrent.rs
+++ src/torrent.rs
@@ -1,8 +1,7 @@
-use serde::{
-    de::{self, Visitor},
-    Deserialize, Deserializer, Serialize,
-};
-use std::fmt;
+use serde::{Deserialize, Serialize};
+use sha1::{Digest, Sha1};
+
+pub use hashes::Hashes;
 
 /// Metainfo files (also known as .torrent files)
 #[allow(unused)]
@@ -35,6 +34,19 @@ pub struct Info {
     pub keys: Keys,
 }
 
+impl Info {
+    pub fn hash(&self) -> [u8; 20] {
+        let info_encoded =
+            serde_bencode::to_bytes(&self).expect("re-encode info section should be fine");
+        let mut hasher = Sha1::new();
+        hasher.update(&info_encoded);
+        hasher
+            .finalize()
+            .try_into()
+            .expect("GenericArray<_, 20> == [_; 20]")
+    }
+}
+
 /// There is a key `length` or a key `files`, but not both or neither.
 #[allow(unused)]
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -62,49 +74,52 @@ pub struct File {
     pub path: Vec<String>,
 }
 
-#[allow(unused)]
-#[derive(Debug, Clone)]
-pub struct Hashes(pub Vec<[u8; 20]>);
-struct HashesVisitor;
+mod hashes {
+    use serde::de::{self, Deserialize, Deserializer, Visitor};
+    use serde::ser::{Serialize, Serializer};
+    use std::fmt;
 
-impl<'de> Visitor<'de> for HashesVisitor {
-    type Value = Hashes;
+    #[derive(Debug, Clone)]
+    pub struct Hashes(pub Vec<[u8; 20]>);
+    struct HashesVisitor;
 
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("a byte string whose length is a multiple of 20")
-    }
-
-    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        if v.len() % 20 != 0 {
-            return Err(E::custom(format!("length is {}", v.len())));
+    impl<'de> Visitor<'de> for HashesVisitor {
+        type Value = Hashes;
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a byte string whose length is a multiple of 20")
         }
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            if v.len() % 20 != 0 {
+                return Err(E::custom(format!("length is {}", v.len())));
+            }
 
-        Ok(Hashes(
-            v.chunks_exact(20)
-                .map(|slice| slice.try_into().expect("guaranteed to be length 20"))
-                .collect(),
-        ))
+            Ok(Hashes(
+                v.chunks_exact(20)
+                    .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                    .collect(),
+            ))
+        }
     }
-}
 
-impl<'de> Deserialize<'de> for Hashes {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_bytes(HashesVisitor)
+    impl<'de> Deserialize<'de> for Hashes {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(HashesVisitor)
+        }
     }
-}
 
-impl Serialize for Hashes {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let single_slice = self.0.concat();
-        serializer.serialize_bytes(&single_slice)
+    impl Serialize for Hashes {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let single_slice = self.0.concat();
+            serializer.serialize_bytes(&single_slice)
+        }
     }
 }
